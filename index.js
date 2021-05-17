b2c4ec84dc405da106d57e194221219de644fa58
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-17 10:13:30 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+let dodger = document.getElementById("dodger")
+
+document.addEventListener("keydown", function(event) {
+    console.log(event);
+  });
+
+function moveDodgerLeft(){
+        let leftNumbers = dodger.style.left.replace("px", "")
+        let left = parseInt(leftNumbers, 10)
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`
+        }
+      }
+
+//Dodgers right style is not defined so instead of defining right we can use left
+
+function moveDodgerRight(){
+        let rightNumbers = dodger.style.left.replace("px", "")
+        let right = parseInt(rightNumbers, 10)
+        if (right < 360) {
+          dodger.style.left = `${right + 1}px`
+        }
+      }
+
+document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft()
+        } else if (e.key === "ArrowRight"){
+            moveDodgerRight()
+        }})
